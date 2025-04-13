496e6b21819b88c23ea836f8f51720f4897edcbd
Mohamed maruf kureshi <mohamedmarufkureshikureshi@gmail.com>
2025-04-13 17:14:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const display = document.getElementById("display");
 const buttons = document.querySelectorAll("button");
-const bgVideo = document.getElementById("bgVideo").addEventListener("ended",()=>{
-video.pause();
-video.currentTime -=0.01;
 
-})
 
 let currentInput = "";
 let resetNext = false;
